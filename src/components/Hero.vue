5b405412d3970b389b867a5b5bff4c6b40ef4fac
jrttk <phasugmool_j@silpakorn.edu>
2021-02-03 06:54:14 +0000

    Updating packages and content.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,10 +9,21 @@
       <p
         data-sal="slide-up"
         data-sal-delay="200"
-      >Greetings, I am Jirattikarn a UI/UX Designer and a Full-Stack Developer based in Bangkok, Thailand. I'm also a design student who studying Digital Communication Design at Silpakorn University International College.</p>
+        v-html="message"
+      ></p>
       <div data-sal="slide-up" data-sal-delay="300">
         <a href="mailto:jirattikarn@mailbox.org" class="button">Drop me an email</a>
       </div>
     </div>
   </section>
 </template>
+
+<script>
+import { Vue, Provide } from 'vue-property-decorator'
+export default class Hero extends Vue {
+    // data: {
+    //     message: ``
+    // }
+    @Provide() message = `Greetings, I am Jirattikarn a UI/UX Designer, Full-Stack Developer & a dog lover based in Bangkok, Thailand. I'm also a design student studying Digital Communication Design at Silpakorn University International College and the co-founder at <a href="https://anotter.co">anotter.</a> a lovely small software house.`
+}
+</script>
